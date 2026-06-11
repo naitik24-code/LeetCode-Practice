a3e52c9287c72e3c9ebfcd1dc9b012743750206b
naitik24-code <naitikpathak2005@gmail.com>
2026-06-11 10:28:31 +0530

    Time: 0 ms (100%), Space: 12.5 MB (26.98%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,17 +1,14 @@
 class Solution(object):
     def gcdOfStrings(self, str1, str2):
-        
-        # Step 1: Check valid pattern
+
         if str1 + str2 != str2 + str1:
             return ""
-        
-        # Step 2: Custom GCD function
-        def gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
-        
-        gcd_len = gcd(len(str1), len(str2))
-        
-        # Step 3: Return result
+
+        gcd_len = self.gcd(len(str1), len(str2))
+
         return str1[:gcd_len]
+
+    def gcd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
