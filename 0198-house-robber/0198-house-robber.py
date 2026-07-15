24e6730886457acd78e11e1ddd531226882aa9f7
naitik24-code <naitikpathak2005@gmail.com>
2026-07-15 14:40:10 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.74%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rob(self, nums):
+        prev1 = prev2 = 0
+        for num in nums:
+            temp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = temp
+
+        return prev1
